a742f7c29a50c03722fd4e4b37fc5ea75c8d88e1
Tiago Vieira <tiagosvieira10@gmail.com>
2023-10-24 15:36:35 -0300

    object da api conectado

--- src/app/modules/wheater/page/wheater-home/wheater-home.component.ts
+++ src/app/modules/wheater/page/wheater-home/wheater-home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-wheater-home',
   templateUrl: './wheater-home.component.html',
-  styleUrls: ['./wheater-home.component.scss']
+  styleUrls: []
 })
-export class WheaterHomeComponent {
+export class WheaterHomeComponent implements OnInit {
+  initialCityName = 'São Paulo';
+  weatherDatas!: any
 
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  getWeatherDatas(cityName: string): void {
+    this.weatherService.getWeatherDatas(cityName)
+    .subscribe({
+      next: (response) => {
+        console.log(response)
+      },
+      error: (error) => console.log(error),
+    });
+  }
 }
