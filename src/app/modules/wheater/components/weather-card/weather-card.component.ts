92dab7b4e56d44fe7b39a032d91fac4417c99048
Tiago Vieira <tiagosvieira10@gmail.com>
2023-10-31 10:52:13 -0300

    component weather home configurado

--- src/app/modules/wheater/components/weather-card/weather-card.component.ts
+++ src/app/modules/wheater/components/weather-card/weather-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { weatherDatas } from 'src/app/models/interfaces/WeatherDatas';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: []
 })
-export class WeatherCardComponent {
-
+export class WeatherCardComponent implements OnInit{
+  ngOnInit(): void {
+    console.log('dados recebidos do componente pai', this.weatherDatasInput);
+  }
+  @Input() weatherDatasInput!: weatherDatas;
 }
